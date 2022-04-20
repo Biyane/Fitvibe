156a5fb166ddd694b2734f96e6032a991ef8b606
kemel <kemaemaaa@gmail.com>
2022-04-20 18:47:19 +0600

    refactor registration activity

--- app/src/main/java/com/example/fitvibe/registration/presentation/RegistrationBaseActivity.kt
+++ app/src/main/java/com/example/fitvibe/registration/presentation/RegistrationBaseActivity.kt
@@ -8,19 +8,10 @@ import com.example.fitvibe.databinding.ActivityRegistrationBaseBinding
 class RegistrationBaseActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityRegistrationBaseBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityRegistrationBaseBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        initView()
-    }
-
-    private fun initView() {
-        supportFragmentManager.beginTransaction()
-            .replace(
-                R.id.registration_activity_fragment_container,
-                RegistrationSmsFragment.newInstance(),
-                RegistrationSmsFragment.TAG
-            ).commit()
     }
 }
